303243624e1d28068c00c0c7ad321486e0c98740
carmi <carmi@CERMINA_MAGED>
2025-06-25 02:06:20 +0300

    Product Module -Entities Configurations

---
+++ LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Products/ProductConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using LinkDev.Talabat.Domain.Entities.Products;
+using LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Base;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Products
+{
+    public class ProductConfiguration : BaseAuditableEntityConfiguration<Product, int>
+    {
+        public override void Configure(EntityTypeBuilder<Product> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .IsRequired();
+
+            builder.Property(P => P.Price)
+                .HasColumnType("decimal(9,2)");
+
+
+            builder.HasOne(product => product.Brand)
+                .WithMany()
+                .HasForeignKey(Product => Product.BrandId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(product => product.Category)
+                .WithMany()
+                .HasForeignKey(product => product.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+
+
+        }
+    }
+
+}
--- LinkDev.Talabat.Infrastructure.Persistence/StoreDbContext.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/StoreDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,13 @@ namespace LinkDev.Talabat.Infrastructure.Persistence
     {
         public StoreDbContext(DbContextOptions<StoreDbContext>options):base(options)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+          //  modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
 
         }
 
