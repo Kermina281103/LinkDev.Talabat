4a378faed99c9fb41632e39156043696a69e9649
carmi <carmi@CERMINA_MAGED>
2025-06-25 14:17:22 +0300

    Unif Of Work

---
+++ LinkDev.Talabat.Domain/Contract/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Domain.Contract
+{
+    public interface IUnitOfWork:IAsyncDisposable
+    {
+        public IGenericRepository<Product,int> ProductRepository { get; }
+        public IGenericRepository<ProductBrand,int> Brands { get; }
+        public IGenericRepository<ProductCategory,int> Categories { get; }
+
+
+        Task<int> CompleteAsync();
+    }
+}
---
+++ LinkDev.Talabat.Infrastructure.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using LinkDev.Talabat.Domain.Contract;
+using LinkDev.Talabat.Domain.Entities.Products;
+using LinkDev.Talabat.Infrastructure.Persistence.Repositories;
+
+
+namespace LinkDev.Talabat.Infrastructure.Persistence.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreDbContext _dbContext;
+        private readonly Lazy<IGenericRepository<Product, int>> _productRepository;
+        private readonly Lazy<IGenericRepository<ProductBrand, int>> _brandRepository;
+        private readonly Lazy<IGenericRepository<ProductCategory, int>> _categoryRepository;
+        public UnitOfWork(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _productRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenericRepository<Product, int>(_dbContext));
+            _brandRepository = new Lazy<IGenericRepository<ProductBrand, int>>(() => new GenericRepository<ProductBrand , int>(_dbContext));
+            _categoryRepository = new Lazy<IGenericRepository<ProductCategory, int>>(() => new GenericRepository<ProductCategory, int>(_dbContext));  
+        }
+
+        public IGenericRepository<Product, int> ProductRepository => _productRepository.Value;
+        public IGenericRepository<ProductBrand, int> Brands => _brandRepository.Value;
+        public IGenericRepository<ProductCategory, int> Categories => _categoryRepository.Value;
+
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
