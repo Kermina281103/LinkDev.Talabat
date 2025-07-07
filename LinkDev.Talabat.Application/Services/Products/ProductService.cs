0d5ebeb7a4d8d830f4573d9b62cae93082c0b55a
carmi <carmi@CERMINA_MAGED>
2025-07-07 12:06:47 +0300

    ServiceManager

---
+++ LinkDev.Talabat.Application/Services/Products/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using LinkDev.Talabat.Application.Abstraction.Services.Products;
+using LinkDev.Talabat.Domain.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Application.Services.Products
+{
+    class ProductService:IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductService(IUnitOfWork unitOfWork , IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+
+    }
+}
---
+++ LinkDev.Talabat.Application/Services/ServiceManager.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using LinkDev.Talabat.Application.Abstraction.Services;
+using LinkDev.Talabat.Application.Abstraction.Services.Products;
+using LinkDev.Talabat.Application.Services.Products;
+using LinkDev.Talabat.Domain.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Application.Services
+{
+
+    namespace LinkDev.Talabat.Application.Services
+    {
+        public class ServiceManager : IServiceManager
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IMapper _mapper;
+            private readonly Lazy<IProductService> _productService;
+
+            public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper)
+            {
+                _unitOfWork = unitOfWork;
+                _mapper = mapper;
+                // Fixed: removed asterisks and used correct field name
+                _productService = new Lazy<IProductService>(() => new ProductService(_unitOfWork,_mapper));
+            }
+
+            public IProductService ProductService
+            {
+                get
+                {
+                    return _productService.Value;
+                }
+            }
+        }
+    }
+}
