85594996d070221bb53406b38545fcb1d92fa190
carmi <carmi@CERMINA_MAGED>
2025-08-13 01:58:45 +0300

    Basket Entities

---
+++ LinkDev.Talabat.Domain/Entities/Basket/Basket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Domain.Entities.Basket
+{
+    public class Basket:BaseEntity<string>
+    {
+        public required IEnumerable<BasketItem> Items { get; set; }
+    }
+}
