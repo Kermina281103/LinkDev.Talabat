1683b9bc061dd6dbc067c47265d8b27cd3aa3174
carmi <carmi@CERMINA_MAGED>
2025-09-01 00:31:00 +0300

    Account Controller -Login and Register endpoints

---
+++ LinkDev.Talabat.APIs.Controllers/Account/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using LinkDev.Talabat.APIs.Controllers.Base;
+using LinkDev.Talabat.Application.Abstraction.Models.Auth;
+using LinkDev.Talabat.Application.Abstraction.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.APIs.Controllers.Account
+{
+    public class AccountController(IServiceManager serviceManager):BaseApiController
+    {
+        [HttpPost("login")] // Post: /api/Account/login
+
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var result = await serviceManager.AuthService.LoginAsync(model);
+
+            return Ok(result);
+        }
+
+        [HttpPost("register")] // Post: /api/Account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+        {
+            var result = await serviceManager.AuthService.RegisterAsync(model);
+            return Ok(result);
+        }
+    }
+}
