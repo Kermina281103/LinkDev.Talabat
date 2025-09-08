8415e9dbf7364e7eabb50c3242a161a2c3361082
carmi <carmi@CERMINA_MAGED>
2025-09-08 14:11:19 +0300

    Make Changes

--- LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrderController.cs
+++ LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrderController.cs
@@ -15,7 +15,7 @@ namespace LinkDev.Talabat.APIs.Controllers.Controllers.Orders
     [Authorize]
     public class OrderController(IServiceManager serviceManager):BaseApiController
     {
-        [HttpPost]//Post : /api/Orders
+        [HttpPost]//Post : /api/Order
         public async Task<ActionResult<OrderToReturnDto>> CreateOrder(OrderToCreateDto orderDto)
         {
             var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
@@ -23,6 +23,33 @@ namespace LinkDev.Talabat.APIs.Controllers.Controllers.Orders
             var result = await serviceManager.OrderService.CreateOrderAsync(buyerEmail!, orderDto);
             return Ok(result);
 
+        }
+
+        [HttpGet] //Get: /api/Order
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetOrderForUser()
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrderService.GetOrdersForUserAsync(buyerEmail!);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")] //Get: /api/Orders/{id}
+
+        public async Task<ActionResult<OrderToReturnDto>> GetOrder(int id)
+        {
+            var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+            var result = await serviceManager.OrderService.GetOrderByIdAsync(buyerEmail!, id);
+            return Ok(result);
+        }
+
+
+        [HttpGet("deliveryMethod")] //Get: /api/Orders/{id}
+
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDto>>> GetDeliveryMethod()
+        {
+           
+            var result = await serviceManager.OrderService.GetDeliveryMethodAsync();
+            return Ok(result);
         }
     }
 }
--- LinkDev.Talabat.Application.Abstraction/Models/AddressDto.cs
+++ LinkDev.Talabat.Application.Abstraction/Models/AddressDto.cs
@@ -8,5 +8,10 @@ namespace LinkDev.Talabat.Application.Abstraction.Models
 {
     public class AddressDto
     {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Street { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
     }
 }
